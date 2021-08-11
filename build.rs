99376f3c4dac65cef3d6c77b51224fb4696688f4
twallick <twallick@vivsoft.io>
2021-08-11 12:24:53 +0200

    added generated protobuf

--- build.rs
+++ build.rs
@@ -1,10 +1,17 @@
+//extern crate dotenv;
+
+//use dotenv;
+use std::env;
 use std::path::PathBuf;
 
-fn main()->Result<(),Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //dotenv::dotenv().expect("Failed to read .env file");
+
+    //let out_pb_dir = PathBuf::from(env::var("PROTOGEN").unwrap());
     let out_pb_dir = PathBuf::from("./protogen");
-    tonic_build::configure().out_dir(out_pb_dir)
+    tonic_build::configure()
+        .out_dir(out_pb_dir)
         .compile(&["proto/rocket-launch.proto"], &["proto"])
         .unwrap();
     Ok(())
- }
- 
+}
